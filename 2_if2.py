69d61a32420a7fac5084fba6c8380cdb2cd645bb
klyuevaa <python.klyuev@gmail.com>
2021-11-25 11:07:19 +0300

    2+

--- 2_if2.py
+++ 2_if2.py
@@ -14,13 +14,26 @@
   и выводя на экран результаты
 
 """
+def strings(str1,str2):
+  if type(str1)==str and type(str2)==str:
+    if str1==str2:
+      return 1
+    elif len(str1)>len(str2):
+      return 2
+    elif str2 == 'learn':
+      return 3
+  else: return 0
+    
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    print(strings('asdfdsa',234))
+    print(strings('learn','learn'))
+    print(strings('learn','abc'))
+    print(strings('abc','learn'))
     
 if __name__ == "__main__":
     main()
