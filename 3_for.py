e35528643047b6bd40637f11f84d08a7f32fab75
klyuevaa <python.klyuev@gmail.com>
2021-11-25 13:09:04 +0300

    +3_вар.1

--- 3_for.py
+++ 3_for.py
@@ -15,13 +15,34 @@
 * Посчитать и вывести суммарное количество продаж всех товаров
 * Посчитать и вывести среднее количество продаж всех товаров
 """
+def summ_sold(stock):
+  summ = 0
+  for price in stock:
+    summ += price
+  return summ
+
+def avg_sold(stock):
+  summ = summ_sold(stock)
+  return summ / len(stock)
+
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-    
+  stock = [
+  {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
+  {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+  {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]}
+  ]
+
+  sold_all=0
+  count=0  
+  for name_of_prod in stock:
+    product = name_of_prod['product']
+    sold = summ_sold(name_of_prod['items_sold'])
+    print(product,sold)
+    sold_all += sold
+    count += len(name_of_prod['items_sold'])
+  print(sold_all)
+  print(count)
+
 if __name__ == "__main__":
     main()
